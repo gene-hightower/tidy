841079da08fa7adf9b848a349914418bdc6e6f46
Gene Hightower <gene@digilicious.com>
2018-08-13 13:48:29 -0700

    hello, world

--- tidy-test.cpp
+++ tidy-test.cpp
@@ -1,3 +1,38 @@
 #include "tidy.hpp"
 
-int main() { tidy::doc doc{}; }
+int main()
+{
+  auto const input = "<title>Foo</title><p>Foo!";
+
+  tidy::buffer output;
+  tidy::buffer errbuf;
+
+  int rc = -1;
+
+  tidy::doc tdoc{};
+
+  std::cout << "Tidying:\t" << input << '\n';
+
+  auto ok = tdoc.opt_set_bool(TidyXhtmlOut, true);
+  if (ok)
+    rc = tdoc.set_error_buffer(errbuf);
+  if (rc >= 0)
+    rc = tdoc.parse_string(input);
+  if (rc >= 0)
+    rc = tdoc.clean_and_repair();
+  if (rc >= 0)
+    rc = tdoc.run_diagnostics();
+  if (rc > 1)
+    rc = (tdoc.opt_set_bool(TidyForceOutput, true) ? rc : -1);
+  if (rc >= 0)
+    rc = tdoc.save_buffer(output);
+
+  if (rc >= 0) {
+    if (rc > 0)
+      std::cout << "\nDiagnostics:\n\n" << errbuf;
+
+    std::cout << "\nAnd here is the result:\n\n" << output;
+  }
+  else
+    std::cerr << "A severe error (" << rc << ") occurred.\n";
+}
--- tidy.hpp
+++ tidy.hpp
@@ -1,9 +1,39 @@
 #ifndef TIDY_DOT_HPP_INCLUDED
 #define TIDY_DOT_HPP_INCLUDED
 
+#include <iostream>
+
 #include <tidy.h>
+#include <tidybuffio.h>
 
 namespace tidy {
+
+class doc;
+
+class buffer {
+public:
+  buffer(buffer const&) = delete;
+  buffer(buffer&&) = delete;
+  buffer& operator=(buffer const&) = delete;
+  buffer& operator=(buffer&&) = delete;
+
+  buffer()
+    : buf_{0}
+  {
+  }
+  ~buffer() { tidyBufFree(&buf_); }
+
+  friend doc;
+
+private:
+  TidyBuffer buf_;
+
+  friend std::ostream& operator<<(std::ostream& os, buffer const& buf)
+  {
+    return os << buf.buf_.bp;
+  }
+};
+
 class doc {
 public:
   doc(doc const&) = delete;
@@ -11,9 +41,25 @@ public:
   doc& operator=(doc const&) = delete;
   doc& operator=(doc&&) = delete;
 
-  doc() { doc_ = tidyCreate(); }
-
+  doc()
+    : doc_{tidyCreate();}
+  {
+  }
   ~doc() { tidyRelease(doc_); }
+
+  bool opt_set_bool(TidyOptionId id, bool val)
+  {
+    return yes == tidyOptSetBool(doc_, id, val ? yes : no);
+  }
+
+  int set_error_buffer(buffer& err)
+  {
+    return tidySetErrorBuffer(doc_, &err.buf_);
+  }
+  int parse_string(char const* input) { return tidyParseString(doc_, input); }
+  int clean_and_repair() { return tidyCleanAndRepair(doc_); }
+  int run_diagnostics() { return tidyRunDiagnostics(doc_); }
+  int save_buffer(buffer& output) { return tidySaveBuffer(doc_, &output.buf_); }
 
 private:
   TidyDoc doc_;
