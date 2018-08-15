6ec46cbcf6bc332796a28ff6cb014878044f9f6c
Gene Hightower <gene@digilicious.com>
2018-08-14 18:44:25 -0700

    wrap some more, not too sure this adds a lot, but the names are consistent

--- tidy-test.cpp
+++ tidy-test.cpp
@@ -1,8 +1,154 @@
 #include "tidy.hpp"
 
+#include <stdio.h>
+
+using namespace std::string_literals;
+
+void dumpNode(tidy::doc& tdoc, TidyNode tnod, int indent)
+{
+  for (auto child = tidy::get_child(tnod); child;
+       child = tidy::get_next(child)) {
+    char const* n = tidy::node_get_name(child);
+    char const* name{nullptr};
+
+    // clang-format off
+    switch (tidy::node_get_type(child)) {
+    case TidyNode_Root:    name = "Root†";                   break;
+    case TidyNode_DocType: name = "DOCTYPE†";                break;
+    case TidyNode_Comment: name = "Comment†";                break;
+    case TidyNode_ProcIns: name = "Processing Instruction†"; break;
+    case TidyNode_Text:    name = "Text†";                   break;
+    case TidyNode_CDATA:   name = "CDATA†";                  break;
+    case TidyNode_Section: name = "XML Section†";            break;
+    case TidyNode_Asp:     name = "ASP†";                    break;
+    case TidyNode_Jste:    name = "JSTE†";                   break;
+    case TidyNode_Php:     name = "PHP†";                    break;
+    case TidyNode_XmlDecl: name = "XML Declaration†";        break;
+
+    case TidyNode_Start:
+    case TidyNode_End:
+    case TidyNode_StartEnd:
+    default:               name = n;                         break;
+    }
+    if (n == nullptr) {
+      auto typ = tidy::node_get_type(child);
+      printf("\%*.*sType:  \%d\n", indent, indent, " ", typ);
+    }
+
+    // clang-format on
+    CHECK_NOTNULL(name);
+
+    if (("a"s == name) || ("link"s == name) || ("script"s == name)) {
+
+      printf("\%*.*sNode:  \%s\n", indent, indent, " ", name);
+      {
+        printf("\%*.*sName:  \%s\n", indent, indent, " ", n ? n : "nullptr");
+      }
+
+      auto value = tdoc.node_get_value(child);
+      if (value) {
+        printf("\%*.*sValue: \%s\n", indent, indent, " ", value->bp());
+      }
+      auto text = tdoc.node_get_text(child);
+      if (text) {
+        printf("\%*.*sText: \%s\n", indent, indent, " ", text->bp());
+      }
+    }
+
+    dumpNode(tdoc, child, indent + 4);
+  }
+}
+
+void dumpDoc(tidy::doc& tdoc)
+{
+  dumpNode(tdoc, tdoc.get_root(), 0);
+  printf("\n");
+}
+
+void dumpBody(tidy::doc& tdoc)
+{
+  dumpNode(tdoc, tdoc.get_body(), 0);
+  printf("\n");
+}
+
 int main()
 {
-  auto const input = "<title>Foo</title><p>Foo!";
+  auto const input = u8R"EOS(
+<!DOCTYPE html>
+<html>
+
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Challenge 2</title>
+  <meta name="description" content="This site has lots of amusing gotchas for people who attempt to scrape it!
+">
+
+  <link rel="stylesheet" href="http://triplebyte.github.io/web-crawler-test-site/css/main.css">
+  <link rel="canonical" href="http://triplebyte.github.io//web-crawler-test-site/test2/">
+  <link rel="alternate" type="application/rss+xml" title="Triplebyte web crawler test site" href="http://triplebyte.github.io//web-crawler-test-site
+/feed.xml">
+</head>
+
+
+  <body>
+
+    <header class="site-header">
+
+  <div class="wrapper">
+
+    <a class="site-title" href="http://triplebyte.github.io/web-crawler-test-site/">Triplebyte web crawler test site</a>
+
+
+  </div>
+
+</header>
+
+
+    <div class="page-content">
+      <div class="wrapper">
+        <h1 id="challenge-2">Challenge 2</h1>
+
+<p>I am a paragraph!</p>
+
+<p>Sometimes I am <a href="http://triplebyte.github.io/web-crawler-test-site/test2/cynical.html">overly cynical</a>, but sometimes I am <a href="htt
+p://triplebyte.github.io/web-crawler-test-site/test2/page2.html#na&amp;#xEFve">overly naïve</a>.</p>
+
+
+      </div>
+    </div>
+
+    <footer class="site-footer">
+
+  <div class="wrapper">
+
+    <h2 class="footer-heading">Triplebyte web crawler test site</h2>
+
+    <div class="footer-col-wrapper">
+      <div class="footer-col footer-col-1">
+        <ul class="contact-list">
+          <li>Triplebyte web crawler test site</li>
+          <li><a href="mailto:buck@triplebyte.com">buck@triplebyte.com</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-col footer-col-3">
+        <p>This site has lots of amusing gotchas for people who attempt to scrape it!
+</p>
+      </div>
+    </div>
+
+  </div>
+
+</footer>
+
+
+  </body>
+
+</html>
+)EOS";
 
   tidy::buffer output;
   tidy::buffer errbuf;
@@ -11,13 +157,21 @@ int main()
 
   tidy::doc tdoc{};
 
-  std::cout << "Tidying:\t" << input << '\n';
+  // std::cout << "Tidying:\t" << input << '\n';
 
+  // auto ok = tdoc.opt_set_bool(TidyXhtmlOut, true);
   auto ok = tdoc.opt_set_bool(TidyXhtmlOut, true);
+  if (ok)
+    ok = tdoc.opt_set_int(TidyWrapLen, 0xFFFFFFFF);
+  if (ok)
+    ok = tdoc.opt_set_bool(TidyMark, false);
   if (ok)
     rc = tdoc.set_error_buffer(errbuf);
   if (rc >= 0)
     rc = tdoc.parse_string(input);
+
+  dumpDoc(tdoc);
+
   if (rc >= 0)
     rc = tdoc.clean_and_repair();
   if (rc >= 0)
@@ -26,6 +180,8 @@ int main()
     rc = (tdoc.opt_set_bool(TidyForceOutput, true) ? rc : -1);
   if (rc >= 0)
     rc = tdoc.save_buffer(output);
+
+  dumpDoc(tdoc);
 
   if (rc >= 0) {
     if (rc > 0)
--- tidy.hpp
+++ tidy.hpp
@@ -2,6 +2,7 @@
 #define TIDY_DOT_HPP_INCLUDED
 
 #include <iostream>
+#include <optional>
 
 #include <tidy.h>
 #include <tidybuffio.h>
@@ -15,22 +16,44 @@ class doc;
 class buffer {
 public:
   buffer(buffer const&) = delete;
-  buffer(buffer&&) = delete;
   buffer& operator=(buffer const&) = delete;
-  buffer& operator=(buffer&&) = delete;
+
+  // move only
+  buffer(buffer&& other)
+  {
+    buffer_ = other.buffer_;
+    other.buffer_.bp = nullptr;
+  }
+  buffer& operator=(buffer&& rhs)
+  {
+    buffer_ = rhs.buffer_;
+    rhs.buffer_.bp = nullptr;
+    return *this;
+  }
 
   buffer()
-    : buf_{0}
+    : buffer_{0}
   {
   }
-  ~buffer() { tidyBufFree(&buf_); }
+  ~buffer()
+  {
+    if (buffer_.bp)
+      tidyBufFree(&buffer_);
+  }
+
+  unsigned char const* bp() const { return buffer_.bp; }
 
 private:
-  TidyBuffer buf_;
+  TidyBuffer buffer_;
+
+  explicit buffer(TidyBuffer b)
+    : buffer_(b)
+  {
+  }
 
   friend std::ostream& operator<<(std::ostream& os, buffer const& buf)
   {
-    return os << buf.buf_.bp;
+    return os << buf.buffer_.bp;
   }
 
   friend doc;
@@ -54,17 +77,50 @@ public:
     return yes == tidyOptSetBool(doc_, id, val ? yes : no);
   }
 
+  bool opt_set_int(TidyOptionId id, int val) const
+  {
+    return yes == tidyOptSetInt(doc_, id, val);
+  }
+
   // clang-format off
-  int set_error_buffer(buffer& err)   const { return tidySetErrorBuffer(doc_, &err.buf_); }
+  int set_error_buffer(buffer& err)   const { return tidySetErrorBuffer(doc_, &err.buffer_); }
   int parse_string(char const* input) const { return tidyParseString   (doc_, input); }
   int clean_and_repair()              const { return tidyCleanAndRepair(doc_); }
   int run_diagnostics()               const { return tidyRunDiagnostics(doc_); }
-  int save_buffer(buffer& output)     const { return tidySaveBuffer    (doc_, &output.buf_); }
+  int save_buffer(buffer& output)     const { return tidySaveBuffer    (doc_, &output.buffer_); }
   // clang-format on
+
+  TidyNode get_root() const { return tidyGetRoot(doc_); }
+  TidyNode get_body() const { return tidyGetBody(doc_); }
+
+  std::optional<buffer> node_get_value(TidyNode node) const
+  {
+    TidyBuffer value{0};
+    if (tidyNodeGetValue(doc_, node, &value)) {
+      return buffer(value);
+    }
+    return {};
+  }
+
+  std::optional<buffer> node_get_text(TidyNode node) const
+  {
+    TidyBuffer text{0};
+    if (tidyNodeGetValue(doc_, node, &text)) {
+      return buffer(text);
+    }
+    return {};
+  }
+
+  TidyDoc get_tdoc() { return doc_; }
 
 private:
   TidyDoc doc_;
 };
+
+TidyNode get_child(TidyNode n) { return tidyGetChild(n); }
+TidyNode get_next(TidyNode n) { return tidyGetNext(n); }
+char const* node_get_name(TidyNode n) { return tidyNodeGetName(n); }
+TidyNodeType node_get_type(TidyNode n) { return tidyNodeGetType(n); }
 
 } // namespace tidy
 
